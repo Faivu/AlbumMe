92b084cc608763901e83bd2bc15fec7c12cff0e4
Safir <boukhalfasafir@gmail.com>
2025-11-18 18:41:55 +0000

    feat: implement a functional create album feature

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Artist;
+use App\Entity\Genre;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+
+
+class AppFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        // Genres data
+        $electronica = new Genre();
+        $electronica->setName('Electronica');
+        $manager->persist($electronica);
+        $folk = new Genre();
+        $folk->setName('Folk');
+        $manager->persist($folk);
+        $pop = new Genre();
+        $pop->setName('Pop');
+        $manager->persist($pop);
+
+        $popRock = new Genre();
+        $popRock->setName('Pop rock');
+        $manager->persist($popRock);
+        $indiePop = new Genre();
+        $indiePop->setName('Indie pop');
+        $manager->persist($indiePop);
+
+        // Artists data
+        $sia = new Artist();
+        $sia->setName('Sia');
+        $manager->persist($sia);
+        $imagineDragons = new Artist();
+        $imagineDragons->setName('Imagine Dragons');
+        $manager->persist($imagineDragons);
+
+        // User data
+        $user = new User();
+        $user->setEmail('test@gmail.com');
+        $user->setUsername('test');
+        // Hashing the user's password
+        $password = $this->hasher->hashPassword($user, 'test123');
+        $user->setPassword($password);
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
--- src/Form/AlbumType.php
+++ src/Form/AlbumType.php
@@ -9,24 +9,60 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AlbumType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // artist and genres not ordered, I can do that later since UX is not a priority rn
         $builder
             ->add('title')
-            ->add('trackList')
+            // Changed from json to textarea for now, I need to make it work firt the assignment 1
+            ->add('trackList', TextareaType::class, [
+                'help' => 'Enter each track on a new line',
+                'attr' => ['rows' => 10],
+                'required' => false,
+            ])
             ->add('artist', EntityType::class, [
                 'class' => Artist::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
             ])
             ->add('genres', EntityType::class, [
                 'class' => Genre::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
                 'multiple' => true,
+                // Temporaty for this first assignment 
+                'expanded' => true
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Create Album'
             ])
         ;
+        // A data transformer since trackList has an array and can't process strings directly, I just don't have time for js that won't my mark better in A1.
+        $builder->get('trackList')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($tracksAsArray): string {
+                    if (empty($tracksAsArray)) {
+                        return '';
+                    }
+                    return implode("\n", $tracksAsArray);
+                },
+
+                // From string to array, addModelTransformer needs to functions for the two ways, so it knows which one to use and when
+                function ($tracksAsString): array {
+
+                    if (empty($tracksAsString)) {
+                        return [];
+                    }
+                    $tracks = explode("\n", str_replace("\r\n", "\n", $tracksAsString));
+
+                    // Trim 
+                    return array_filter(array_map('trim', $tracks));
+                }
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
