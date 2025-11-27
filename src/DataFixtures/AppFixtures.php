7396a66b3cfc4967c371eae0a9a46435c629a0a5
Safir <boukhalfasafir@gmail.com>
2025-11-27 13:20:19 +0000

    feat: add faker via composer then use it to create 3 test users, one album with 60 reviews from different users and more genres

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,13 +2,15 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Album;
 use App\Entity\Artist;
 use App\Entity\Genre;
+use App\Entity\Review;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use App\Entity\User;
-
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
@@ -58,6 +60,54 @@ class AppFixtures extends Fixture
         $password = $this->hasher->hashPassword($user, 'test123');
         $user->setPassword($password);
         $manager->persist($user);
+
+        // Three users and an album with 60 reviews
+        $faker = Factory::create();
+        $users = [];
+        for ($i = 1; $i <= 3; $i++) {
+            $user = new User();
+            $user->setEmail("test$i@example.com");
+            $user->setUsername("TestUser$i");
+            $user->setPassword($this->hasher->hashPassword($user, 'password123'));
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+        $genres = [];
+        foreach (['Rock', 'Pop', 'Jazz', 'Electronic'] as $name) {
+            $genre = new Genre();
+            $genre->setName($name);
+            $manager->persist($genre);
+            $genres[] = $genre;
+        }
+
+        $artist = new Artist();
+        $artist->setName('The Test Band');
+        $artist->addGenre($genres[0]);
+        $manager->persist($artist);
+
+        $album = new Album();
+        $album->setTitle('The Greatest Hits');
+        $album->setArtist($artist);
+        $album->setCreator($users[0]); // Created by User 1
+        $album->setTrackList(['Track 1', 'Track 2', 'Track 3']);
+        $album->addGenre($genres[0]);
+        $manager->persist($album);
+        for ($i = 0; $i < 60; $i++) {
+            $review = new Review();
+            $review->setRating($faker->numberBetween(1, 5));
+            $review->setComment($faker->paragraph()); // Random text
+            
+            // Link to the one album
+            $review->setAlbum($album);
+            
+            // Link to a random user from our list
+            $randomUser = $faker->randomElement($users);
+            $review->setReviewer($randomUser);
+
+            $manager->persist($review);
+        }
+
 
         $manager->flush();
     }
